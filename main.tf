2909ac54907bd821988b3475fea3309149be19f2
jjcallis <jjcallis@users.noreply.github.com>
2022-01-24 10:28:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "osodevops"
+
+    workspaces {
+      name = "client_infrastructure"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
